7e21112fe9f551c2682d884da6f4ae8d44b23cec
Lina Yang <t886515@hotmail.com>
2018-07-24 22:17:16 -0500

    - solving linting errors

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,6 +5,10 @@ module.exports = {
   },
   extends: ['plugin:vue/essential', '@vue/airbnb'],
   rules: {
+    'import/no-extraneous-dependencies': [
+      'error',
+      { devDependencies: false, optionalDependencies: false, peerDependencies: false },
+    ],
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
   },
